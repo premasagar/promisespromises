83d7fae9ee54e4a766da52ebbeb43b91f749d2ac
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-02 17:31:21 +0000

    added get multiple image example

--- examples/promiseImage.js
+++ examples/promiseImage.js
@@ -1,16 +1,16 @@
-var	firstPromise, secondPromise, thirdPromise,
-	images = [];
+var firstPromise, secondPromise, thirdPromise,
+    images = [];
 
 firstPromise  = jQuery.get('http://farm8.staticflickr.com/7010/6751627459_685330f95d_m.jpg', addImage);
 secondPromise = jQuery.get('http://farm8.staticflickr.com/7173/6774879243_d81744f248_m.jpg', addImage);
 thirdPromise  = jQuery.get('http://farm8.staticflickr.com/7031/6726223445_436d7ac38a_m.jpg', addImage);
 
 function addImage(data) {
-	imags.push(data);
+    imags.push(data);
 }
 
 jQuery
-	.when(firstPromise, secondPromise, thirdPromise)
-	.then(function(){
-		// display images
-	});
+    .when(firstPromise, secondPromise, thirdPromise)
+    .then(function(){
+        // display images
+    });
