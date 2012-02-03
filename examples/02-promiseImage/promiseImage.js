909f5ddefcf8bb9bc44a937ba6a3bdcc3d48f637
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-03 11:10:37 +0000

    added multiple image example

---
+++ examples/02-promiseImage/promiseImage.js
@@ -0,0 +1,35 @@
+function promiseImage(src){
+    var $img = jQuery('<img>'),
+        img = $img[0],
+        deferred = jQuery.Deferred();
+
+    $img.on('load', function(){
+           deferred.resolve(img);
+       })
+       .on('error', function(){
+           deferred.reject(img);
+       })
+       .attr('src', src);
+
+    return deferred.promise();
+}
+
+function display(img1, img2) {
+    jQuery(img1).add(img2)
+        .hide()
+        .appendTo('.output')
+        .fadeIn();
+}
+
+function error(img) {
+    jQuery('.output')
+        .append('Error loading image: ' + img.src);
+}
+
+jQuery('button').click(function(){
+    var promiseImage1 = promiseImage('x.jpg'),
+        promiseImage2 = promiseImage('bar.jpg');
+
+    jQuery.when(promiseImage1, promiseImage2)
+        .then(display, error);
+});
