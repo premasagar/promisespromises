ac610783b9d7c80907a5015c69e2967fdf136516
Premasagar Rose <p@dharmafly.com>
2012-02-09 17:59:51 +0000

    updated demos

--- examples/05-Ajax/Ajax.js
+++ examples/05-Ajax/Ajax.js
@@ -1,12 +1,47 @@
-function display(amount) {
-    $('.output').append('Number of attendees: ' + amount);
+function promiseImage(src){
+    var $img = jQuery('<img>'),
+        img = $img[0],
+        deferred = jQuery.Deferred();
+
+    $img.on('load', function(){
+           deferred.resolve(img);
+       })
+       .on('error', function(){
+           deferred.reject(img);
+       })
+       .attr('src', src);
+
+    return deferred.promise();
 }
 
-jQuery('button').click(function(){
-  var url = 'http://lanyrd.asyncjs.com/2011/asyncjs-security/attendees/';
+function display(/* each image node is passed in as an argument */) {
+    jQuery(arguments)
+        .hide()
+        .appendTo('.output')
+        .fadeIn(1000);
+}
 
-  jQuery.getJSON(url + "?callback=?")
-      .then(function(data){
-          display(data.attendees.length);
-      });
-});
+function error(img) {
+    jQuery('.output')
+        .append('Error loading image: ' + img.src);
+}
+
+function getLanyrdAttendees() {
+  var url = 'http://lanyrd.asyncjs.com/2011/asyncjs-security/attendees/',
+      images = [];
+
+    jQuery.getJSON(url + "?callback=?")
+        .then(function(data){
+            
+            jQuery.each(data.attendees, function(i, attendees){
+                var img = attendees.user.avatar_url;
+                images.push(promiseImage(img));
+            });
+
+            jQuery.when.apply(jQuery, images)
+                .then(display, error);
+        });
+
+}
+
+jQuery('button').click(getLanyrdAttendees);
